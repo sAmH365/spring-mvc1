f0893f26606ead8f7613a2f33fdb966d0443233f
sAmH365 <samh365@sAmH365ui-MacBookPro.local>
2023-03-09 00:21:57 +0900

    HTTP 요청 데이터 -API 메시지 바디 -JSON

---
+++ src/main/kotlin/hello/servelet/basic/HelloData.kt
@@ -0,0 +1,7 @@
+package hello.servelet.basic
+
+class HelloData {
+
+    var username = ""
+    var age = 0
+}
---
+++ src/main/kotlin/hello/servelet/basic/request/RequestBodyJsonServlet.kt
@@ -0,0 +1,30 @@
+package hello.servelet.basic.request
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import hello.servelet.basic.HelloData
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.util.StreamUtils
+import java.nio.charset.StandardCharsets
+
+@WebServlet(name = "RequestBodyJsonServlet", urlPatterns = ["/request-body-json"])
+class RequestBodyJsonServlet: HttpServlet() {
+
+    private val objectMapper: ObjectMapper = ObjectMapper()
+
+
+    override fun service(request: HttpServletRequest, response: HttpServletResponse) {
+        val inputStream = request.inputStream
+        val messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8)
+
+        println("messageBody = $messageBody")
+        val helloData = objectMapper.readValue(messageBody, HelloData::class.java)
+
+        println("helloData.username = ${helloData.username}")
+        println("helloData.age = ${helloData.age}")
+
+        response.writer.write("ok!")
+    }
+}
